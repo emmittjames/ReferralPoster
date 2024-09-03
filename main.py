a8ede4adec246fad19d072246933ba8f06f8abf8
emmittjames <emmittjames1@gmail.com>
2024-09-03 02:26:18 -0400

    added sportsbook subreddits to the mix

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ user_agent = config["reddit"]["user_agent"]
 username = config["reddit"]["username"]
 password = config["reddit"]["password"]
 
-csv_file_path = "subreddits.csv"
-
 def read_subreddits(csv_file):
     subreddits = []
     with open(csv_file, mode="r", newline="") as file:
@@ -71,23 +69,41 @@ def delete_posts_in_subreddit(reddit, selected_subreddit):
                 print(f"Deleting post: {submission.title} in r/{selected_subreddit['subreddit']}")
                 submission.delete()
 
-def create_post(selected_subreddit):
+def get_title_and_body(promo_subreddit):
+    if promo_subreddit:
+        title = "Get $250 on FanDuel Sportsbook"
+        body = (
+            "Referral link: https://refer.sportsbook.fanduel.com/#/land/4bcd834a-beeb-4b2c-98b0-2fac6a35527e\n\n"
+            "The only terms are that you must deposit $10 and make any bet with that $10 to qualify!\n\n"
+            "You will then receive $50 through my referral link and another $200 as a new customer bonus from FanDuel."
+        )
+    else:
+        title = "Referral code for $50 in bonus bets"
+        body = (
+            "Referral link: https://refer.sportsbook.fanduel.com/#/land/4bcd834a-beeb-4b2c-98b0-2fac6a35527e\n\n"
+            "The only terms are that you must deposit $10 and make any bet. After that you will be awarded $50!"
+        )
+    return title, body
+
+def create_post(selected_subreddit, promo_subreddit):
     reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent, username=username, password=password)
 
     print(f"Deleting old posts in {selected_subreddit}...")
     delete_posts_in_subreddit(reddit, selected_subreddit)
 
     subreddit = reddit.subreddit(selected_subreddit["subreddit"])
-    title = "Get $250 on FanDuel Sportsbook"
-    body = (
-        "Referral link: https://refer.sportsbook.fanduel.com/#/land/4bcd834a-beeb-4b2c-98b0-2fac6a35527e\n\n"
-        "The only terms are that you must deposit $10 and make any bet with that $10 to qualify!\n\n"
-        "You will then receive $50 through my referral link and another $200 as a new customer bonus from FanDuel."
-    )
+    title, body = get_title_and_body(promo_subreddit)
     subreddit.submit(title=title, selftext=body)
     return subreddit.subscribers
 
 def main():
+    if random.random() < 0.99:
+        promo_subreddit = False
+        csv_file_path = "sportsbook_subreddits.csv"
+    else:
+        promo_subreddit = True
+        csv_file_path = "promo_subreddits.csv"
+
     subreddits = read_subreddits(csv_file_path)
 
     print("Calculating weights...")
@@ -97,7 +113,7 @@ def main():
     print(f"Selected subreddit: {selected_subreddit["subreddit"]}")
 
     print("Creating post...")
-    members = create_post(selected_subreddit)
+    members = create_post(selected_subreddit, promo_subreddit)
 
     print("Updating CSV file...")
     update_subreddit(selected_subreddit, members)
